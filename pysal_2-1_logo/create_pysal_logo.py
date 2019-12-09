784e17cf90c0514ab2da73958090c1a556d808ae
James Gaboardi <jgaboardi@gmail.com>
2019-12-09 13:03:43 -0500

    correcting typo

--- pysal_2-1_logo/create_pysal_logo.py
+++ pysal_2-1_logo/create_pysal_logo.py
@@ -30,7 +30,7 @@ Requirements:
         info -- https://mplus-fonts.osdn.jp/about-en.html
         download -- https://osdn.net/projects/mplus-fonts/releases/<RELEASE>
             The files created with the initial push for this file were run
-            release <62344>. The current release for download is <p14454>.
+            on release <62344>. The current release for download is <p14454>.
     ImageTricks (for favicon creation)
         https://www.belightsoft.com/products/imagetricks/
 
