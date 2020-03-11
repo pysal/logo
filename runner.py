c882c2c44c30c44ada1560651e0dad68f87a02d4
James Gaboardi <jgaboardi@gmail.com>
2020-03-11 13:51:43 -0400

    runner with transparent background

--- runner.py
+++ runner.py
@@ -6,20 +6,20 @@ run the following from the command line within the top directory:
     
 """
 
-from logo import create_logo, create_favicon, canon2020_theme_light
+from logo import create_logo, create_favicon, canon2020_theme_transparent
 
 # Create the default canon2020/PySAL2020 logo
-# and favicons with a light background
+# and favicons with a transparent background
 
-canon2020_theme_light.update({"move_to":None})
+canon2020_theme_transparent.update({"move_to":None})
 base_name = "pysal_logo"
-logo_name = base_name + "_light"
+logo_name = base_name
 
 # create the logo
-create_logo(logo_name, **canon2020_theme_light)
+create_logo(logo_name, **canon2020_theme_transparent)
 
 # create favicons
 fcon_res = ["32", "48", "64"]
-canon2020_theme_light["concept_text"] = ""
+canon2020_theme_transparent["concept_text"] = ""
 for fr in fcon_res:
-    create_favicon(base_name, resolution=fr, **canon2020_theme_light)
+    create_favicon(base_name, resolution=fr, **canon2020_theme_transparent)
